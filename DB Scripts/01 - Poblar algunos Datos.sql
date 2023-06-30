ef48a0cfab10cc68f3fe2748d88ff9db621ac6c1
Ramiro D'Accorso <ramiro.daccorso@gmail.com>
2023-06-30 02:27:28 -0300

    script para carga de datos demo

---
+++ DB Scripts/01 - Poblar algunos Datos.sql
@@ -0,0 +1,22 @@
+insert into Categorias (Descripcion)
+values ('Alimento Perros'),('Alimento Gatos')
+
+insert into Marcas (Descripcion)
+values ('Peedigree'),('Natual Choice'),('Estampa'),('Purina')
+
+insert into Articulos (Nombre,Descripcion,DescripcionLarga,IdMarca,IdCategoria,Precio)
+values ('Pedigree','Alimento Perro','Para perritos lindos',1,1,500),
+('Natural DOG','Alimento Perro','Para perritos lindos',1,1,1500),
+('Estampa pupis','Alimento Perro','Para perritos lindos',1,1,300),
+('Purina Excelent','Alimento Michis','Para michis lindos',2,2,750),
+('Cat Chow ','Alimento Michis','Para perritos lindos',2,2,500)
+
+
+insert into Imagenes (IdArt,ImagenUrl)
+values 
+(1,'https://jumboargentina.vtexassets.com/arquivos/ids/760144/Alimento-Para-Perros-Pedigree-Adulto-15-Kg-1-16855.jpg?v=638048145796470000'),
+(2,'https://delkoalimentos.com.ar/contenido/lineas/1.png'),
+(3,'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/961/189/products/d_858741-mla32272538095_092019-o-4b3adf38dd99cc956f16386572719704-640-0.jpg'),
+(4,'https://mla-s1-p.mlstatic.com/851749-MLA49052565471_022022-F.jpg'),
+(5,'https://hiperlibertad.vteximg.com.br/arquivos/ids/193270-600-600/CATCHOW-ADLTPESCX85G-1-7699.jpg?v=637820590240100000')
+
